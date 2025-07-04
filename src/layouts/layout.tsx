cd62746d71d63c0a30aa946a1593f9d0056540d2
Yannick MATH <yannick_math@yahoo.fr>
2025-07-04 16:23:00 +0200

    WIP

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./footer";
 
 export default function Layout() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="w-full h-full flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <Header />
       <Outlet /> {/* Ici s’affichera Home ou About */}
       <Footer />
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import Loader from "../components/loader";
 import { useGetProductsByCategoryQuery } from "../services/productsByCategory";
 
 export default function Home() {
@@ -7,15 +8,17 @@ export default function Home() {
     isLoading,
   } = useGetProductsByCategoryQuery("2");
 
+  const { name, description, price } = products ? products[0] : {};
+  console.log(isLoading);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-600">
       <h1 className="text-4xl font-bold mb-4">Welcome to FastFood</h1>
       <p className="text-lg text-gray-700 mb-8">
         Your favorite food delivered fast!
       </p>
       <button className="btn btn-primary">Order Now</button>
       <div className="mt-8">
-        {isLoading && <p>Loading...</p>}
+        {isLoading && <Loader />}
         {error && (
           <p>
             Error loading products:{" "}
@@ -30,7 +33,7 @@ export default function Home() {
           <ul className="list-disc">
             {products.map((product) => (
               <li key={product.id} className="mb-2">
-                {product.name} - ${product.price} - {product.description}
+                {name} - ${price} - {description}
               </li>
             ))}
           </ul>
